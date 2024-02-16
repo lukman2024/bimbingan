6d64dda74c8cd4044d4e5087ca79ced46a0e4a4b
lukman2024 <rukumang.com@gmail.com>
2024-02-16 09:37:43 +0700

    alert

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("selamat datang");
+var lagi = true;
+while (lagi == true) {
+  var nama = prompt("masukan nama:");
+  alert("halo " + nama);
+
+  lagi = confirm("coba lagi?");
+}
+alert("terimakasih..");
