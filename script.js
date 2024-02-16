8fe1add3da112653c63628fc62861ec42428bcd8
lukman2024 <rukumang.com@gmail.com>
2024-02-16 11:35:44 +0700

    pengulangan tabel

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var nilaiAwal = 1;
-while (nilaiAwal <= 100) {
-  console.log("Hello world " + nilaiAwal + "x");
-  nilaiAwal++;
+var jmlAngkot = 10;
+var noAngkot = 1;
+
+while (noAngkot < jmlAngkot) {
+  console.log("angkot no. " + noAngkot + " beroperasi densgn baik.");
+  noAngkot++;
 }
