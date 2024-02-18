73146e7bd98a3bcbc91460df95cba7f6b0843259
lukman2024 <rukumang.com@gmail.com>
2024-02-19 05:30:49 +0700

    membuat dan menjalankan function

--- script.js
+++ script.js
@@ -1,38 +1,15 @@
-var tanya = true;
-while (tanya) {
-  var p = prompt("pilih : batu, gunting, kertas");
+function jumlahVolumeDuaKubus(a, b) {
+  var volumeA;
+  var voluneB;
+  var total;
 
-  var comp = Math.random();
+  volumeA = a * a * a;
+  voluneB = b * b * b;
 
-  if (comp < 0.34) {
-    comp = "batu";
-  } else if (comp >= 0.34 && comp < 0.67) {
-    comp = "gunting";
-  } else {
-    comp = "kertas";
-  }
+  total = volumeA + voluneB;
 
-  var hasil = "";
-  if (p == comp) {
-    hasil = "SERI";
-  } else if (p == "kertas") {
-    // if( comp == 'gajah' ) {
-    //     hasil = 'KALAH!';
-    // } else {
-    //     hasil = 'MENANG!';
-    // }
-    hasil = comp == "batu" ? "MENANG!" : "KALAH!";
-  } else if (p == "gunting") {
-    hasil = (comp = "batu") ? "KALAH!" : "MENANG!";
-  } else if (p == "batu") {
-    hasil = (comp = "kertas") ? "KALAH!" : "MENANG!";
-  } else {
-    hasil = "memasukkan pilihan yang salah";
-  }
-
-  alert("kamu memilih : " + p + " computer memilih : " + comp + "\nkamu " + hasil);
-
-  tanya = confirm("main lagi?");
+  return total;
 }
 
-alert("terimakasih sudah bermain");
+alert(jumlahVolumeDuaKubus(8, 3));
+alert(jumlahVolumeDuaKubus(5, 10));
