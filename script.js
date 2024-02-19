43869259221759a342d10c836daf004237fa5556
lukman2024 <rukumang.com@gmail.com>
2024-02-19 11:46:20 +0700

    array manipulation

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-function faktoriala(n) {
-  if (n == 0) return 1;
-  return n * faktoriala(n - 1);
-}
+//manipulasi array
 
-faktoriala(5);
+//1. menambah isi array
+// var arr = [];
+// arr[0] = "sandhika";
+// arr[1] = "galih";
+// arr[2] = "nova";
+// arr[6] = "doddy";
+
+//console.log(arr);
+
+//2. menghapus isi array
+// var arr = ["shandhika","galih","nova"]
+// arr[1] = undefined;
+// console.log(arr);
+
+//menampilkan isi array
+// var arr = ["shandhika", "galih", "nova"];
+
+// for(var i = 0; 1 < arr.length; 1++ ) {
+// console.log('mahasiswa ke-' + (i+1) + ' : ' + arr[i]);
+// }
+
+//methot pada array
+var arr = ["shandika", "galih", "nofa"];
+//1.join
+// console.log(arr.join(' - '))
+
+//2.push dan pop
+// arr.push('dody','fitri');
+// arr.pop();
+// arr.pop();
+// console.log(arr.join(' - '))
+
+//3.unshift dan shift
+arr.unshift("doddy");
+arr.shift();
+console.log(arr.join(" - "));
