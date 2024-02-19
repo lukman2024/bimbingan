0006b7d17132e1113d0176ccf275ca2ca6d804cc
lukman2024 <rukumang.com@gmail.com>
2024-02-19 16:52:00 +0700

    membuat object

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-var mhs = {
-  nama: "lukman",
-  umur: 18,
-  ips: [3.0, 2.5, 3.2],
-  alamat: {
-    jalan: "batu karang",
-    kota: "kelawu",
-    provinsi: "jawa selatan",
-  },
+//membuat object
+//object literal
+var mhs1 = {
+  nama: "lukman h",
+  nrp: "09876543",
+  email: "mohlukk.com.com",
+  jurusan: "tehnik informatika",
 };
+var mhs2 = {
+  nama: "rukumang h",
+  nrp: "12345678",
+  email: "Rukumang.com.com",
+  jurusan: "tehnik informatika",
+};
+
+//function declaration
+function buatObjectMahasiswa(nama, nrp, email, jurusan) {
+  var mhs = {};
+  mhs.nama = nama;
+  mhs.nrp = nrp;
+  mhs.email = email;
+  mhs.jurusan = jurusan;
+  return mhs;
+}
+var mhs3 = buatObjectMahasiswa("lukman", "0987879", "lukman.com", "teknik pangan");
+
+//constructor
+function Mahasiswa(nama, nrp, email, jurusan) {
+  this.nama = nama;
+  this.nrp = nrp;
+  this.email = email;
+  this.jurusan = jurusan;
+}
+var mhs4 = new Mahasiswa("namkul", "093498", "namkul.com", "teknik mesin");
