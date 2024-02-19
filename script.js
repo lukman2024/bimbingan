3b6d4a6be207989bcf9b2b63476095de84bd1eec
lukman2024 <rukumang.com@gmail.com>
2024-02-19 15:47:24 +0700

    membuat program pengelola angkot2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var penumpang = ["sandhika", undefined, "doddy"];
+var penumpang = [];
 var tambahPenumpang = function (namaPenumpang, penumpang) {
   if (penumpang.length == 0) {
     penumpang.push(namaPenumpang);
@@ -17,4 +17,21 @@ var tambahPenumpang = function (namaPenumpang, penumpang) {
       }
     }
   }
+};
+
+var hapusPenumpang = function (namaPenumpang, penumpang) {
+  if (penumpang.length == 0) {
+    console.log("angkot masih kosong.");
+  } else {
+    for (var i = 0; i < penumpang.length; i++) {
+      if (penumpang[i] == namaPenumpang) {
+        penumpang[i] = undefined;
+        return penumpang;
+      } else if (i == penumpang.length - 1) {
+        console.log(namaPenumpang + "tidak ada di dalam angkot.");
+        return penumpang;
+      }
+    }
+  }
+  return penumpang;
 };
