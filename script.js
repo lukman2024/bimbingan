c67cc29090142822c091d52f6c29c7155a71ef47
lukman2024 <rukumang.com@gmail.com>
2024-02-19 14:06:03 +0700

    foreach & map

--- script.js
+++ script.js
@@ -22,7 +22,7 @@
 // }
 
 //methot pada array
-var arr = ["shandika", "galih", "nofa"];
+// var arr = ["shandika", "galih", "nofa"];
 //1.join
 // console.log(arr.join(' - '))
 
@@ -38,11 +38,40 @@ var arr = ["shandika", "galih", "nofa"];
 // console.log(arr.join(" - "));
 
 //4.splice
-arr.splice(1, 2, "doody", "fitri");
-console.log(arr.join(" - "));
+// arr.splice(1, 2, "doody", "fitri");
+// console.log(arr.join(" - "));
 
 //5.slice
-var arr = ["shandhika", "galih", "nofa", "doddy", "fitri"];
-var arr2 = arr.slice(1, 4);
-console.log(arr.join(" - "));
-console.log(arr.join(" - "));
+// var arr = ["shandhika", "galih", "nofa", "doddy", "fitri"];
+// var arr2 = arr.slice(1, 4);
+// console.log(arr.join(" - "));
+// console.log(arr.join(" - "));
+
+//6.foreach
+// var angka = [1, 2, 3, 4, 5, 6, 7, 8];
+// var nama = ["shandhika", "galih", "nova"];
+// for( var i =0; i < angka.length; i++ ) {
+//   console.log(angka[1]);
+// }
+
+// angka.forEach(function (e) {
+//   console.log(e);
+// });
+
+// nama.forEach(function (e, i) {
+//   console.log("mahasiswa ke-" + (i + 1) + " adalah : " + e);
+// });
+
+//7.map
+// var angka = [1, 2, 5, 3, 6, 8, 4];
+// var angka2 = angka.map(function (e) {
+//   return e * 2;
+// });
+// console.log(angka2.join(' - '));
+
+//8. sort
+var angka = [1, 2, 5, 10, 20, 7, 4, 6];
+angka.sort(function (a, b) {
+  return a - b;
+});
+console.log(angka.join(" - "));
