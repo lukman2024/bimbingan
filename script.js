f9ce2c8fb3035a822b8632d79d139ecbeca475e9
lukman2024 <rukumang.com@gmail.com>
2024-02-20 16:45:47 +0700

    manipulation DOM2

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-// const judul = document.getElementById('judul');
-// judul.innerHTML = '<em>lukman </em>';
+//DOM manipulation
+//buat elemen baru
+const pBaru = document.createElement("p");
+const textPBaru = document.createTextNode("paragraf baru");
+//simpan tulisan ke dalam paragraf
+pBaru.appendChild(textPBaru);
 
-// const sectionA =document.querySelector('section#a');
-// sectionA innerHTML = '<div><p>paragrafi</p></div>'
+//simpan pBaru di akhir Section A
+const SectionA = document.getElementById("a");
+SectionA.appendChild(pBaru);
 
-// const judul =document.querySelector('#judul');
-// judul.computedStyleMap.color = 'lightblue';
-// judul.style.backgroundColor = 'salmon';
+const liBaru = document.createElement("li");
+const textLiBaru = document.createTextNode("Item Baru");
+liBaru.appendChild(textLiBaru);
 
-// const judul = document.getElementsByTagName('h1')[0];
-// judul.setAttribute('name', 'lukman')
-// const a = document.querySelector('section a');
+const ul = document.querySelector("section#b ul");
+const li2 = ul.querySelector("li:nth-child(2)");
+
+ul.insertBefore(liBaru, li2);
+
+const link = document.getElementsByTagName("a")[0];
+
+SectionA.removeChild(link);
+
+const sectionB = document.getElementById("b");
+const p4 = sectionB.querySelector("p");
+
+const h2Baru = document.createElement("h2");
+const teksH2Baru = document.createTextNode("judul baru!");
+
+h2Baru.appendChild(teksH2Baru);
+
+sectionB.replaceChild(h2Baru, p4);
+
+pBaru.style.backgroundColor = "lightgreen";
+liBaru.style.backgroundColor = "lightgreen";
+h2Baru.style.backgroundColor = "lightgreen";
