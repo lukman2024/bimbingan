fb6850e1e75f7f4a034f8b96232369bdf5795d75
lukman2024 <rukumang.com@gmail.com>
2024-02-20 09:01:55 +0700

    object this

--- script.js
+++ script.js
@@ -1,34 +1,26 @@
-//membuat object
-//object literal
-var mhs1 = {
-  nama: "lukman h",
-  nrp: "09876543",
-  email: "mohlukk.com.com",
-  jurusan: "tehnik informatika",
-};
-var mhs2 = {
-  nama: "rukumang h",
-  nrp: "12345678",
-  email: "Rukumang.com.com",
-  jurusan: "tehnik informatika",
-};
+//this
+// var a = 10;
+// console.log(window.a);
 
-//function declaration
-function buatObjectMahasiswa(nama, nrp, email, jurusan) {
-  var mhs = {};
-  mhs.nama = nama;
-  mhs.nrp = nrp;
-  mhs.email = email;
-  mhs.jurusan = jurusan;
-  return mhs;
-}
-var mhs3 = buatObjectMahasiswa("lukman", "0987879", "lukman.com", "teknik pangan");
+//cara 1-function declaration
+// function hallo() {
+//   console.log(this);
+//   console.log("hallo");
+// }
+// this.hallo();
 
-//constructor
-function Mahasiswa(nama, nrp, email, jurusan) {
-  this.nama = nama;
-  this.nrp = nrp;
-  this.email = email;
-  this.jurusan = jurusan;
+//cara 2-object literal
+// var obj = { a: 10, nama: "lukman" };
+// obj.hallo = function () {
+//   console.log(this);
+//   console.log("hallo");
+// };
+// obj.hallo();
+
+//cara 3-constructour
+function Hallo() {
+  console.log(this);
+  console.log("hallo");
 }
-var mhs4 = new Mahasiswa("namkul", "093498", "namkul.com", "teknik mesin");
+var obj1 = new Hallo();
+var obj2 = new Hallo();
