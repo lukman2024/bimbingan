6ff786d2c5c85b8bdb27d1edcc594353e7c21454
lukman2024 <rukumang.com@gmail.com>
2024-02-20 11:35:26 +0700

    DOM selection

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-//membuat object
-function Angkot(sopir, trayek, penumpang, kas) {
-  this.sopir = sopir;
-  this.trayek = trayek;
-  this.penumpang = penumpang;
-  this.kas = kas;
+//dom selectio
+//getElementById
+const judul = document.getElementById("judul");
+judul.style.color = "red";
+judul.style.backgroundColor = "#ccc";
+judul.innerHTML = "lukmen";
 
-  this.penumpangNaik = function (namaPenumpang) {
-    this.penumpang.push(namaPenumpang);
-    return this.penumpang;
-  };
+// document.getElementsByTagName()
+const p = document.getElementsByTagName("p");
 
-  this.penumpangTurun = function (namaPenumpang, bayar) {
-    if (this.penumpang.length == 0) {
-      alert("angkot masih kosong");
-      return false;
-    }
-    for (var i = 0; i < this.penumpang.length; i++) {
-      if (this.penumpang[i] == namaPenumpang) {
-        this.penumpang[i] == undefined;
-        this.kas += bayar;
-        return this.penumpang;
-      }
-    }
-  };
+for (let i = 0; i < p.length; i++) {
+  p[i].style.backgroundColor = "lightblue";
 }
 
-var angko1 = new Angkot("lukmen", ["Cichaheum", "cibiru"], [], 0);
-var angko2 = new Angkot("thomas", ["Tom cruis", "Ciroyom"], [], 0);
+const h1 = document.getElementsByTagName("h1")[0];
+h1.stylefontSize = "50px";
+
+//document.getElementsBYclassName
+const p1 = document.getElementsByClassName("p1")[0];
+p1.innerHTML = "ini di ubah dari javascript";
